cd91cef220301cc7692b596eed17ef86cfb1a1c7
Kristian <344849@via.dk>
2023-10-23 17:59:58 +0200

    Inserted a placebo code in this programs GCD class, since that program has not been written yet.

--- Handin_05/Handin_05/src/Program_5_extendedEA/GCD.java
+++ Handin_05/Handin_05/src/Program_5_extendedEA/GCD.java
@@ -1,5 +1,7 @@
 package Program_5_extendedEA;
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 // This is a dependent class whose methods are used by
 // extendedEA
@@ -10,7 +12,17 @@ public class GCD
   public static int findGCD(int a, int b)
   {
     // Copy your favorite GCD method from Program 1 here
-    return 42;
+    //THE BELOW MODEL IS A PLACEHOLDER MODEL FOUND ON THE INTERNET!
+    // if b=0, a is the GCD
+    if (b == 0)
+      return a;
+
+      // call the gcd() method recursively by
+      // replacing a with b and b with
+      // modulus(a,b) as long as b != 0
+    else
+      return findGCD(b, a % b);
+    //THE ABOVE MODEL IS A PLACEHOLDER MODEL FOUND ON THE INTERNET!
   }
 
   public static void main(String[] args)
