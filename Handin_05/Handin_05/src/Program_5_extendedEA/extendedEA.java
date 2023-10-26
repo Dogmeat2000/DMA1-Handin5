631362f825a4cddec391899247799e3153cd516f
Kristian <344849@via.dk>
2023-10-26 22:39:35 +0200

    Finished the documentation for the operation of the EEA method.
    Still missing the Big(O) analysis.

--- Handin_05/Handin_05/src/Program_5_extendedEA/extendedEA.java
+++ Handin_05/Handin_05/src/Program_5_extendedEA/extendedEA.java
@@ -11,6 +11,8 @@ public class extendedEA
     // x is the inverse of a
     // There is only an inverse if gcd(a,b) == 1
 
+
+
     // Implement your method here
 
     /** I commented this part out, since I was failing the pre-built tests. Basically my intention here is to catch when no inverse exists instead of performing the calculation. However I have decided to comment this out for now.*/
@@ -29,120 +31,61 @@ public class extendedEA
       return new int[] {1, 0};
     }
 
-    //Since we are not allowed to pass x and y values downward, we will instead need to find the bottom values and work our way upward in a recurring manner.
+    //Since we are not allowed to pass x and y values downward, we will instead need to find the bottom values and work our way upward in a recursive manner.
     //So at this point we simply call the next iteration of this EEA method, using the values we know will be a and b one level down:
     int[] downwardRecurringIterationResult = EAA(b, (a % b));
+    /**Let us use this calculcation example for the remainder of this method, to illustrate what actually happens at each step:
+     * We use this case as the example: EEA(102,53) -> We want to find 102 mod 53.
+       * 1st iteration: Value of downwardRecurringIterationResults is EEA(53, (102 % 53)) = EEA(53,49)
+       * 2nd iteration: Value of downwardRecurringIterationResults is EEA(49, (53 % 49)) = EEA(49,4)
+       * 3rd iteration: Value of downwardRecurringIterationResults is EEA(4, (49 % 4)) = EEA(49,1)
+       * 4th iteration: Value of downwardRecurringIterationResults is EEA(1, (4 % 1)) = EEA(1,0) <----- HERE THE RECURSIVE LOOP DOWNWARD BREAKS AND BEGINS WORKING UPWARD AGAIN, SINCE b = 0!
+     * We know that for a to be 1 and b to be 0, x and y must each respectively be x = 1 and y = 0. -> And the product of these values should equal the GCD of 1.
+     *
+     * At the 4th iteration the nested calls begin working their way up the recursive ladder. We get these return values:
+       * 4th iteration returns [x=1, y=0]
+       * 3rd iteration proceeds with the below code, using x=1 and y=0.*/
 
     //Since we have reached this line in the code, it means we are now working our way back upwards in our backward recurring sequence.
-    //We now declare and initialize the relevant values for this iterations calculations:
 
+    //We now declare and initialize the relevant values for this iterations calculations:
     //We extract our x and y values from our downward recurring iteration.
-    int x_CurrentIteration = downwardRecurringIterationResult[0];
-    int y_CurrentIteration = downwardRecurringIterationResult[1];
+    int x_PriorIteration = downwardRecurringIterationResult[0];
+    int y_PriorIteration = downwardRecurringIterationResult[1];
 
     //Going back upwards to the next EEA iteration we know that:
-    /** Here we utilize the knowledge that the value a in the current iteration would become the value b in the next downward iteration.
-     * Since there is a multiplicative relationship between ax and by we simply treat the known x/y values as if they were a/b values*/
-    //x, one level up is equal to the current iterations y value.
-    int x_OneIterationUp = y_CurrentIteration;
-    int y_OneIterationUp = x_CurrentIteration - (a / b) * y_CurrentIteration; //The division operation ignores decimal results since it is working on integers. This is intentional here.
+    /** Here we utilize the knowledge we have from how the EEA recursive algorithm works.
+     * We know that by in the current iteration becomes ax in the next.
+        * Example:
+        * 0th iteration: a*x + b*y = 1
+        * 1st iteration: a1*x1 + b1*y1 = 1
+        * From the above relationship equate these two iterations in a recurring manner: a*x + b*y = a1*x1 + b1*y1 = ..... = 1
 
-    return new int[] {x_OneIterationUp, y_OneIterationUp};
+        * Thus we can express this relationship between the 4th and 3rd iteration:
 
+        * 4th iteration returns [x4=1, y4=0], a4=1 and b4=0.
+        *
+        * In order to express the x and y values for the current iteration we work our known values a bit:
+        * a3*x3 + b3*y3 = a4*x4 + b4*y4
+        * a3*x3 + b3*y3 = b3*x4 + (a3 % b3)*y4
+        * a3*x3 + b3*y3 = b3*x4 + (a3 - b3*(a3|b3)) * y4
+        * a3*x3 + b3*y3 = b3*x4 + a3*y4 - b3*(a3-(a3|b3)*y4
+        * a3*x3 + b3*y3 = a3*y4 + b3*x4 - b3*(a3-(a3|b3)*y4
+        * a3*x3 + b3*y3 = a3*y4 + b3*(x4 - (a3|b3)*y4)
+        * Comparing the above coefficents using bezouts theorem we see that for a3, x3 = y4
+          * thus x3 = y4 (or x_current will equal y_prior_iteration)
 
+        * we also see for b3 that y3 = (x4-(a3|b3)*y4)
+          * thus y_current = (x_prior_iteration - (a_current|b_current)*y_prior_iteration)
+     * */
 
+    int x_CurrentIteration = y_PriorIteration;
+    int y_CurrentIteration = x_PriorIteration - (a / b) * y_PriorIteration; //The division operation ignores decimal results since it is working on integers. This is intentional here, since we want the divisor without remainder.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* We now know that there must be a multiplicative inverse. So we begin the algorithm calculations.
-    ax + by (Bezout's theorem)  is equal to  gcd(a, b) or in other terms: Find the inverse of a mod b.
-
-    //Now we check if either a or b is zero.
-
-
-    First we need to assign the user input to proper variables. */
-    /*int original_a = a;
-    int original_b = b;
-    int x = 1;
-    int y = -1; //We begin at minus 1, since we are only using and accepting positive integer input. Thus all solutions will involve a negative factor.
-
-    /*Now we must express our initial x and y values as they are connected to the a and b values.
-    /*For this we need to determine whether a subtraction or addition is proper.
-
-    Example: GCD(102, 53) = GCD(53, rem(102, 53) = GCD(53, 49) -> Expressed with Bezout's theorem as: 49 = 1 * 102 - 1 * 53*/
-
-    //We establish a while loop, that runs until we have iterated through the EEA algorithm until the remainder between the last iterations value of a (prior_a) and b (prior_b) is 0.
-    /*int i1stWhileLoopIteration = 0;
-
-    System.out.println("a1 is : " + original_a);
-    System.out.println("b1 is : " + original_b);
+    //We have now calculated the current iterations x and y values, and can return these upward to the next recursive call, until we reach the initial call.
+    return new int[] {x_CurrentIteration, y_CurrentIteration};
 
 
-
-    while (((original_a * x) + (original_b * y)) != (original_a % original_b) && i1stWhileLoopIteration < 20)
-    {
-      System.out.println("a2 is : " + original_a);
-      System.out.println("b2 is : " + original_a);
-      //This is done through a bruteforce method. Continuously adding/subtraction to/from either x or y until and expression that is equal to the remainder between original_a and original_b is identified.
-      //We check if the first part of bezout's identity (a*x) is larger than twice the size of the second part (b*x). If yes, we decrease the size of y by 1. If no, we increase the size of x by 1.
-
-      if ((original_a * x) + (original_b * y) >= (original_a * x))
-      {
-        y--;
-      }
-      else
-      {
-        x++;
-      }
-
-      System.out.println("Loop iteration #" + i1stWhileLoopIteration + "\nAttempting to calculate: " + original_a + " % " + original_b + " (" + original_a % original_b + ")" + " = " + original_a + "(a) * " + x + "(x) + " + original_b + "(b) * " + y + "(y) + (" + ((original_a * x) + (original_b * y)) + ")\n--------");
-      i1stWhileLoopIteration++;
-    }
-
-    System.out.println("here");
-
-
-
-    int counter = 0;
-    while(original_a * x + original_b * y != 1 && counter < 4)
-    {
-
-      System.out.println("Nested execution #" + counter + "\nAttempting to calculate: " + original_a + " % " + original_b + " (" + original_a % original_b + ")" + " = " + original_a + "(a) * " + x + "(x) + " + original_b + "(b) * " + y + "(y) + (" + ((original_a * x) + (original_b * y)) + ")\n--------");
-
-
-
-      x = (nextIteration[1] * x) + nextIteration[1];
-      System.out.println("\nx is : " + x);
-
-      y = ((nextIteration[0]) + (nextIteration[1] * y) + nextIteration[1]);
-      System.out.println("y is : " + y);
-      //4  =  1 ∗ 53 − 1 ∗ (1 ∗ 102 − 1 ∗ 53)
-      //4  =  1 ∗ 53 − (102 − 53)
-      //4  =  1 ∗ 53 − 1 ∗ 49
-
-      // (rem(34,13)) 8 = 1 * 34 - 2 * 13
-
-      // (rem(13,8)) 5 = 1*13 - 1 * 8
-      // (rem(13,8)) 5 = 1*13 - 1 * (1 * 34 - 2 * 13)
-      /*counter++;
-
-    }
-
-    return new int[] {x, y};*/
   }
 
   // Do not change methods below;
