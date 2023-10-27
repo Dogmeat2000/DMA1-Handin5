e2725c5bd5068531316608574674f18ae49a3e1a
Alperen941 <Alperenozer03@gmail.com>
2023-10-27 19:22:57 +0200

    Finished program 2 & 6

--- Handin_05/Handin_05/src/Program_2_findMax2/max2.java
+++ Handin_05/Handin_05/src/Program_2_findMax2/max2.java
@@ -5,7 +5,11 @@ public class max2
   public int findMax2(int[] input)
   {
     // TODO implement method
-    int highestnumber = 0;
+
+    //Creating an int which will represent our highest number, starting with the first number
+    int highestnumber = input[0];
+
+    //Inside the loop we compare the int which represent the higest number with the current number in the loop, and set the number to the hightest number int, if it is higher
     for (int i = 0; i<input.length;i++)
     {
       if (highestnumber < input[i])
@@ -13,6 +17,7 @@ public class max2
         highestnumber = input[i];
       }
     }
+    //At the end, after the loop, we now have our highest number in the variable "highestnumber"
     return highestnumber;
   }
 }
--- Handin_05/Handin_05/src/Program_6_squareMultiply/squareMultiply.java
+++ Handin_05/Handin_05/src/Program_6_squareMultiply/squareMultiply.java
@@ -7,13 +7,23 @@ public class squareMultiply
 
     // Implement method here
 
-    // Creating an int which is our result, starting with "base" mod "modulo".
-    int result = base % modulo;
+    // Creating an int which is our result, starting with 1. the result will remain as the same if the exponent is 0.
+    // a number with the power of 0 equals 1. 1 modulo a number will be 1. This equation only works if the numbers are positive numbers.
+    int result = 1;
 
     // repeating until we reached our exponent
-    for (int i = 1; i < exponent; i++)
+    // instead of using Math.pow(a,b), we use an for loop, where the base will multiply, until it repeated the exponent times.
+    // example: a^6 <=> a*a*a*a*a*a
+    for (int i = 0; i < exponent; i++)
     {
       // every time our base multiplies with an exponent, we take mod modulo
+      // a base with the power of exponent modulo a number, is the same as taking the modulo every time you multiply the base with itself
+      /* example:
+       * a^3 % "mod"
+       * result = a % "mod"
+       * result = result * a % "mod"
+       * result = result * a % "mod"
+       */
       result = (result * base) % modulo;
     }
     // At the end we have our result
