3f116d7abab4ffe94d88c2b7c2a81ff6775cf809
zakaria2899 <61026115+zakaria2899@users.noreply.github.com>
2023-11-17 14:53:06 +0100

    Zakaria

--- DMA1 Handins/Handin_07/src/closestBall/closestBall.java
+++ DMA1 Handins/Handin_07/src/closestBall/closestBall.java
@@ -1,12 +1,46 @@
 package closestBall;
 import java.util.*;
-
+/*/ The overall time complexity for the following code is O(m+n).
+When sorting each arraylist it takes O(m*log(m)) and O(n*log(n)), we can re-write this to
+O(m + n).
+ */
 public class closestBall
 {
   public int computeClosest(ArrayList<Integer> players,
       ArrayList<Integer> balls)
   {
-    // TODO implement method
-    return 0;
+
+    // sorting to get the list in ascending order.
+    Collections.sort(players);
+    Collections.sort(balls);
+
+    //Setting the variable to the biggest Integer value.
+    int smallestDifference = Integer.MAX_VALUE;
+
+    // Iterate through the sorted lists.
+    int i = 0, j = 0;
+    while (i < players.size() && j < balls.size())
+    {
+      // Getting the difference the current element at position i and j in the sorted list.
+      int difference = Math.abs(players.get(i) - balls.get(j));
+
+      // Updates the smallestDifference if a smaller difference is found
+      if (difference < smallestDifference)
+      {
+        smallestDifference = difference;
+      }
+
+      if (players.get(i) < balls.get(j))
+      {
+        i++;
+      }
+      else
+      {
+        j++;
+      }
+    }
+
+    return smallestDifference;
   }
 }
+
