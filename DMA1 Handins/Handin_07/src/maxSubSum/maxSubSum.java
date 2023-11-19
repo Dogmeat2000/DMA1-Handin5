8be07716963619b29f52ec6603b59c6a219c04f2
Kristian <344849@via.dk>
2023-11-19 14:04:09 +0100

    Integrated Elhams solution.

--- DMA1 Handins/Handin_07/src/maxSubSum/maxSubSum.java
+++ DMA1 Handins/Handin_07/src/maxSubSum/maxSubSum.java
@@ -7,6 +7,28 @@ public class maxSubSum
   public int findMaxSubSum(ArrayList<Integer> input)
   {
     // TODO implement method
-    return 0;
+
+    // Check if the input list is empty
+    if (input == null || input.isEmpty())
+    {
+      return 0;
+    }
+
+    //Initialize variables to keep track of current sum and maximum sum
+    int currentSum = 0;
+    int maxSum = 0;
+
+    // Iterate through each element in the array
+    for (int num : input)
+    {
+      // update the current sum by adding the current element. If the current sum becomes negative, reset it to 0
+      currentSum = Math.max(0, currentSum + num);
+
+      // Update the maximum sum if the current sum is greater
+      maxSum = Math.max(maxSum, currentSum);
+    }
+
+    // The final result is the maximum sum found
+    return maxSum;
   }
 }
