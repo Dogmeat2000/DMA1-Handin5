ce587999d0fbbcd8f583c5ea28da8d4cc11d4ac6
Alperen941 <Alperenozer03@gmail.com>
2023-11-21 18:04:49 +0100

    Finished the Exercise median, with correct and fast algorithm

--- DMA1 Handins/Handin_08/src/median/Median.java
+++ DMA1 Handins/Handin_08/src/median/Median.java
@@ -4,17 +4,28 @@ import java.io.*;
 import java.util.*;
 
 public class Median {
+    //First we Create our Arraylist which will hold the data
+    private ArrayList<Integer>Input = new ArrayList<>();
+
 
     // Add your private fields here
 
-    public void add(int x) {
 
+    public void add(int x) {
+            //To add a number in the arraylist so that we can save the data
+            Input.add(x);
         // Implement your method to add x to the data structure
     }
 
     public int median() {
 
         // Implement your method to return the median of the numbers added so far
-        return 0;
+
+
+        //Sorting the list from lowest to highest number
+        Collections.sort(Input);
+        //Returning the number in the middle which is our median, if the list size is even, then we have 2 median size/2 and size/2 + 1, and if it is odd its only size/2,
+        //therefore we can always use size/2 to get the median
+        return Input.get(Input.size()/2);
     }
 }
